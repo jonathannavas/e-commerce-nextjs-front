713b0a8521bb040043a7ddcce50a2fcd0eedc763
Jonathan Navas <jonathan.navas9@hotmail.com>
2022-12-08 12:51:41 -0500

    setup meta tags seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import { Typography } from '@mui/material'
+import { ShopLayout } from '../components/layouts'
 export default function Home() {
   return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
+    <ShopLayout
+      title="E-commerce"
+      pageDescription={'Encuentra los mejores productos aqui'}
+    >
+      <Typography variant="h1" component="h1">
+        Tienda
+      </Typography>
+      <Typography variant="h2" component="h2" sx={{ mv: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
   )
 }

c744e9246c8d2a5a75c769277f2b0bd55290a348
Jonathan Navas <jonathan.navas9@hotmail.com>
2022-12-08 19:26:29 -0500

    ProductCard and ProductList added on Home Screen

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { FC } from 'react'
-import { Navbar } from '../ui'
+import { Navbar, SideMenu } from '../ui'
 
 interface Props {
   title: string
@@ -29,6 +29,7 @@ export const ShopLayout: FC<Props> = ({
       <nav>
         <Navbar />
       </nav>
+      <SideMenu />
       {/* { set main info} */}
       <main
         style={{
---
+++ components/products/ProductCard.tsx
@@ -0,0 +1,54 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material'
+import { FC, useMemo, useState } from 'react'
+import { IProduct } from '../../interfaces'
+
+interface Props {
+  product: IProduct
+}
+
+export const ProductCard: FC<Props> = ({ product }: Props) => {
+  const [isHovered, setIsHovered] = useState(false)
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`
+  }, [isHovered])
+
+  return (
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      md={3}
+      onMouseEnter={() => {
+        setIsHovered(true)
+      }}
+      onMouseLeave={() => {
+        setIsHovered(false)
+      }}
+    >
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            className="fadeIn"
+            image={productImage}
+            alt={`${product.title}`}
+          />
+        </CardActionArea>
+      </Card>
+
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>${product.price}</Typography>
+      </Box>
+    </Grid>
+  )
+}
